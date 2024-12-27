d96862dca540b5b6ef9a0d6d7608416fd54e8376
EfosaE <osamwonyiefosa02@gmail.com>
2024-12-27 12:55:10 +0100

    removed redirect from root loader

--- app/root.tsx
+++ app/root.tsx
@@ -36,22 +36,11 @@ export const links: LinksFunction = () => [
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const currentUrl = new URL(request.url);
   const redirectTo = currentUrl.pathname + currentUrl.search; // Preserve the path and query parameters
-  console.log('Running root loader...');
   const response = await getUser(request);
   if (isUser(response)) {
     return new Response(JSON.stringify({ user: response }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  if (isUserWithAccessToken(response)) {
-    const { user, accessToken } = response;
-    // Create session with user and token
-    return createUserSession({
-      request,
-      redirectTo,
-      token: accessToken,
-      user: response.user,
     });
   }
 
